ffa5b674554e0814f141af5f0953bf41ec151ab9
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2020-01-31 17:20:48 +0100

    Add files via upload

---
+++ process_vm.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python3
+from __future__ import print_function
+
+import fnmatch
+import glob
+import json
+import math
+import numpy as np
+import os
+import statistics
+import sys
+from shutil import copyfile
+from operator import itemgetter
+from scipy.stats.mstats import gmean
+
+
+# process usage: supply the path of the top-level browsertime-results directory
+# python process_vm.py {path}
+
+# enable printing of debugging details
+debug = False
+
+class VariantResults:
+  def __init__(self):
+    self.means = []
+    self.stdDevs = []
+    self.relStdDevs = []
+    self.medians = []
+    self.meanSpeedups = []
+    self.medianSpeedups = []
+    
+path = sys.argv[1]
+os.chdir(path);
+files = sorted(glob.glob("*"))
+
+metrics = [['pageLoadTime', "data[0]['statistics']['timings']['pageTimings']", "['pageLoadTime']"],
+
+          ['relLageLoadTime', "data[0]['statistics']['timings']['pageTimings']", "['relPageLoadTime']"],
+
+          ['speedIndex', "data[0]['statistics']['visualMetrics']", "['SpeedIndex']"],
+          ['contentfulSpeedIndex', "data[0]['statistics']['visualMetrics']", "['ContentfulSpeedIndex']"],
+          ['perceptualSpeedIndex', "data[0]['statistics']['visualMetrics']", "['PerceptualSpeedIndex']"],
+          ['visualComplete85', "data[0]['statistics']['visualMetrics']", "['VisualComplete85']"],
+          ['firstVisualChange', "data[0]['statistics']['visualMetrics']", "['FirstVisualChange']"],
+          ['lastVisualChange', "data[0]['statistics']['visualMetrics']", "['LastVisualChange']"],
+          
+          ['firstPaint', "data[0]['statistics']['timings']", "['firstPaint']"],
+          #['firstContentfulPaint', "data[0]['statistics']['timings']", "['timeToContentfulPaint']"],
+
+          ['resourceCount', "data[0]['statistics']['timings']['pageTimings']", "['resourceCount']"],
+          ['resourceDuration', "data[0]['statistics']['timings']['pageTimings']", "['resourceDuration']"],
+
+          ['fetchStart', "data[0]['statistics']['timings']['navigationTiming']", "['fetchStart']"],
+          ['redirectionTime', "data[0]['statistics']['timings']['pageTimings']", "['redirectionTime']"],
+          ['domainLookupStart', "data[0]['statistics']['timings']['navigationTiming']", "['domainLookupStart']"],
+          ['domainLookupEnd', "data[0]['statistics']['timings']['navigationTiming']", "['domainLookupEnd']"],
+          ['domainLookupTime', "data[0]['statistics']['timings']['pageTimings']", "['domainLookupTime']"],
+          
+          ['connectStart', "data[0]['statistics']['timings']['navigationTiming']", "['connectStart']"],
+          ['serverConnectionTime', "data[0]['statistics']['timings']['pageTimings']", "['serverConnectionTime']"],
+
+          ['requestStart', "data[0]['statistics']['timings']['navigationTiming']", "['requestStart']"],
+          ['responseStart', "data[0]['statistics']['timings']['navigationTiming']", "['responseStart']"],
+          ['responseEnd', "data[0]['statistics']['timings']['navigationTiming']", "['responseEnd']"],
+          
+          ['serverResponseTime', "data[0]['statistics']['timings']['pageTimings']", "['serverResponseTime']"],
+
+          ['domInteractiveTime', "data[0]['statistics']['timings']['pageTimings']", "['domInteractiveTime']"],
+          ['relDomInteractiveTime', "data[0]['statistics']['timings']['pageTimings']", "['relDomInteractiveTime']"],
+
+          ['domContentLoadedTime', "data[0]['statistics']['timings']['pageTimings']", "['domContentLoadedTime']"],
+          ['relDomContentLoadedTime', "data[0]['statistics']['timings']['pageTimings']", "['relDomContentLoadedTime']"],
+
+          ['frontEndTime', "data[0]['statistics']['timings']['pageTimings']", "['frontEndTime']"],
+
+          ['duration', "data[0]['statistics']['timings']['navigationTiming']", "['duration']"]]
+
+
+sortedResults = []
+for url in files:
+  os.chdir(url)
+  if debug: print("\nProcessing " + url)
+
+  report = []
+  sessions = glob.glob("*")
+  for k,session in enumerate(sessions):
+      if debug: print("Session: " + session)
+      matches = []
+      for root, dirnames, filenames in os.walk(session):
+        for filename in fnmatch.filter(filenames, 'browsertime.json'):
+            if debug: print("\nfilename " + filename)
+            matches.append(os.path.join(root, filename))
+
+      if not matches:
+        continue
+
+      for r, result in enumerate(matches):
+          if debug: print("\nOpening " + result)
+          with open(result) as f:
+              data = json.load(f)
+
+              instance = {}
+              for metric in metrics:
+                  if eval(metric[2])[0] not in eval(metric[1]):
+                      instance[metric[0] + "Mean"] = 0
+                      instance[metric[0] + "Stddev"] = 0
+                      instance[metric[0] + "RelStddev"] = 0
+                      instance[metric[0] + "Median"] = 0
+                  else:
+                      instance[metric[0] + "Mean"] = eval(metric[1] + metric[2] + "['mean']")
+                      instance[metric[0] + "Stddev"] = eval(metric[1] + metric[2] + "['stddev']")
+                      if float(instance[metric[0] + "Mean"]) == 0:
+                        instance[metric[0] + "RelStddev"] = 0.0
+                      else:
+                        instance[metric[0] + "RelStddev"] = float(instance[metric[0] + "Stddev"]) / float(instance[metric[0] + "Mean"])
+                      instance[metric[0] + "Median"] = eval(metric[1] + metric[2] + "['median']")
+              instance['value']                              = session
+              instance['timestamp']                          = data[0]['info']['timestamp']
+              instance['url']                                = data[0]['info']['url']
+              instance['mode'] = session
+              if debug: print( "report" )
+              report.append(instance)
+
+  sortedResults.append( sorted(report, key=itemgetter('timestamp')) )
+  os.chdir("..")
+
+numRows = 100
+
+# Print data
+speedups = []
+for metric in metrics:
+  meanIndex = metric[0] + "Mean"
+  medianIndex = metric[0] + "Median"
+  stddevIndex = metric[0] + "Stddev"
+  relstddevIndex = metric[0] + "RelStddev"
+
+  print(metric[0] + ":   |", end="")
+  for i,instance in enumerate(sortedResults[0]):
+    if debug: print("\nmetrics for " + instance['url'])
+    if i == 0:
+      print(instance['mode'] + " | | | ", end="")
+    else:
+      print(instance['mode'] + " | | | | | ", end="")
+
+  print(" | ")
+
+  for i,instance in enumerate(sortedResults[0]):
+    if i == 0:
+      print("| Mean | Rel Std Dev | Median | ", end="")
+    else:
+      print("Mean | Rel Std Dev | Improvement relative to baseline | Median | Improvement relative to baseline | ", end="")
+
+  print("")
+  
+  variants = []
+
+  for j,l in enumerate(sortedResults):
+    baseValueMean = 0
+    baseValueMedian = 0
+    if debug : print 
+    print("%-80.80s"% sortedResults[j][0]["url"], end="")
+    print("| ", end="")
+    for i,instance in enumerate(sortedResults[j]):
+      if i > 0 and instance['timestamp'] < sortedResults[j][i-1]['timestamp']:
+        print("ERROR NOT SORTED!!")
+
+      print("%4.0f"% instance[meanIndex]   + "|", end="")
+      #print("%4.0f"% instance[stddevIndex] + "| ", end="")
+      print("%4.4f"% instance[relstddevIndex] + "| ", end="")
+
+      # Update variant values (i.e. columns)
+      if len(variants) <= i: variants.append(VariantResults())
+
+      if instance[meanIndex] != 0:
+        variants[i].means.append(instance[meanIndex])
+      if instance[stddevIndex] != 0:
+        variants[i].stdDevs.append(instance[stddevIndex])
+      if instance[relstddevIndex] != 0:
+        variants[i].relStdDevs.append(instance[relstddevIndex])
+      if instance[medianIndex] != 0:
+        variants[i].medians.append(instance[medianIndex])          
+
+      if i == 0:
+        baseValueMean   = instance[meanIndex]
+        baseValueMedian = instance[medianIndex]
+      else:
+        delta = (baseValueMean-instance[meanIndex])
+        if baseValueMean != 0 and instance[meanIndex] != 0:
+          speedup = (float(baseValueMean) - float(instance[meanIndex]))/float(baseValueMean)
+          speedups.append( speedup )
+        else:
+          speedup = 0
+          speedups.append(0)
+        variants[i].meanSpeedups.append(speedup)
+        print("%6.4f"% speedup + " | " , end="")
+
+      print("   %4.0f"%  instance[medianIndex]   + " |  ", end="")
+      
+      if i == 0:
+        print("", end="")
+      else:
+        if baseValueMedian != 0 and instance[medianIndex] != 0:
+          speedup = (float(baseValueMedian) - float(instance[medianIndex]))/float(baseValueMedian)
+        else:
+          delta = 0
+          speedup = 0
+        variants[i].medianSpeedups.append(speedup)
+        print("%6.4f"% speedup + "", end="")
+        print(" |", end="")
+
+    print("")
+
+  print("Mean | ", end="")
+  for v,v2 in enumerate(variants):
+    baseLineMeanOfMeans = statistics.mean(variants[0].means) if len(variants[0].means) != 0 else 1.0
+    baseLineMeanOfMedians = statistics.mean(variants[0].medians) if len(variants[0].medians) != 0 else 1.0 
+
+    meanOfMeans = statistics.mean(variants[v].means) if len(variants[v].means) != 0 else 0.0 
+    meanOfRelStdDevs = statistics.mean(variants[v].relStdDevs) if len(variants[v].relStdDevs) != 0 else 0.0 
+    print ("%4.2f"% meanOfMeans +  " | ", end="")
+    print ("%4.4f"% meanOfRelStdDevs +  " | ", end="")
+    if (v != 0):
+      meanSpeedup = (baseLineMeanOfMeans - meanOfMeans)/baseLineMeanOfMeans
+      print ("%4.4f"% meanSpeedup + " | ", end="")
+
+    meanOfMedians = statistics.mean(variants[v].medians) if len(variants[v].medians) != 0 else 0.0 
+    print ("%4.2f"% meanOfMedians +  " | ", end="")
+    if (v != 0):
+      medianSpeedup = (baseLineMeanOfMedians - meanOfMedians)/baseLineMeanOfMedians
+      print ("%4.4f"% medianSpeedup + " | ", end="")
+
+  print("")
+  print("")
